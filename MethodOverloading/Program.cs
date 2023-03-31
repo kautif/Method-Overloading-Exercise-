c55edcbd903d8b77139d0ac5238e5dd96f0e5d5e
Autif Kamal <kautif@gmail.com>
2023-03-31 17:38:26 -0500

    Added Overload Methods for Add

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -1,10 +1,39 @@
-﻿namespace MethodOverloading
+﻿using System.Net.NetworkInformation;
+
+namespace MethodOverloading
 {
     public class Program
     {
+
+        public static int Add(int a, int b)
+        {
+            int sum = a + b;
+            Console.WriteLine(sum);
+            return sum;
+        }
+
+        public static double Add(double a, double b) { 
+            double sum = a + b;
+            Console.WriteLine(sum);
+            return sum;
+        }
+
+        public static string Add(int a, int b, bool truth) {
+            int sum = a + b;
+            string currency = "";
+            string dollarAmt = "";
+            if (truth)
+            {
+                currency = sum > 1 ? " dollars" : " dollar";
+                dollarAmt = sum + currency;
+                Console.WriteLine(dollarAmt);
+            }
+            return dollarAmt;
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Add(1.12, 2.11);
         }
     }
 }
